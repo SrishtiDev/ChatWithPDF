a6f79f8cd6626e0cfe65f9d786c7ad69d87f309f
Srishti Rawat <rawat.srishti1010@gmail.com>
2026-07-02 01:51:15 +0530

    chore: setup architecture, gitignore and env templates

--- server/worker.js
+++ server/worker.js
@@ -1,43 +1,61 @@
+import dotenv from "dotenv";
+dotenv.config();
 import { Worker } from "bullmq";
 import { QdrantVectorStore } from "@langchain/qdrant";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import  { Document } from "@langchain/core/documents";
+// import { OpenAIEmbeddings } from "@langchain/openai";
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const worker = new Worker(
   "file-upload-queue",
   async (job) => {
-    console.log(`Job: ${job.id}`);
-    const data = JSON.parse(job.data);
+    try {
+      console.log(`Job: ${job.id}`);
+
+      const data = JSON.parse(job.data);
+
+      const loader = new PDFLoader(data.path);
+      const docs = await loader.load();
+
+      const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+      });
+
+      const splitDocs = await splitter.splitDocuments(docs);
+
+      console.log(`Chunks: ${splitDocs.length}`);
 
 
-    //Load the PDF file from the specified path
-    const loader = new PDFLoader(data.path);
-    const docs = await loader.load();
-
-    //Chunk the PDF into smaller pieces
-    const splitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 300,
-      chunkOverlap: 0,
+      // const embeddings = new OpenAIEmbeddings({
+      //   model: "text-embedding-3-small",
+      //   openAIApiKey: process.env.OPENAI_API_KEY,
+      // });
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+      apiKey: process.env.GOOGLE_API_KEY,
+      model: "gemini-embedding-001",
     });
-    const texts = splitter.splitText(document);
-    console.log(`Number of chunks: ${texts.length}`);
+
+      console.log("Creating collection...");
+
+      await QdrantVectorStore.fromDocuments(splitDocs, embeddings, {
+        url: process.env.QDRANT_URL,
+        collectionName: "langchainjs-testing",
+      });
+
+      console.log("Documents added to Qdrant");
+    } catch (err) {
+      console.error("ERROR:");
+      console.error(err);
+    }
   },
   {
     concurrency: 100,
     connection: {
       host: "localhost",
-      port: "6379",
+      port: 6379,
     },
   },
 );
-
-
-/*Path :data.path
-read the pdf from path,
-chunk the pdf,
-call the openai embedding model for every chunk,
-store the chunk in quandt db
-
-*/
